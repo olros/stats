59f94c4101bb2859773b968a3e3dbbed3f75f92a
Olaf Rosendahl <olafrosendahl@gmail.com>
2023-02-26 14:32:51 +0100

    refactor: file structure

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -34,6 +34,7 @@ const Document = ({ children }: DocumentProps) => {
   return (
     <html data-joy-color-scheme='dark' lang='no'>
       <head>
+        <script data-project='sanntid' data-team='olros' defer src='http://localhost:3000/script.js'></script>
         <link href='/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
         <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
         <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
