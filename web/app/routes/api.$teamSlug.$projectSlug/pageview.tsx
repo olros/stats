a0d66b95acd89db323a1f6ad7228c9cf25950bb0
Olaf Rosendahl <olafrosendahl@gmail.com>
2023-02-28 17:42:47 +0100

    feat: parallell pageview api

--- web/app/routes/api.$teamSlug.$projectSlug/pageview.tsx
+++ web/app/routes/api.$teamSlug.$projectSlug/pageview.tsx
@@ -1,4 +1,4 @@
-import type { Prisma } from '@prisma/client';
+import type { Prisma, Project } from '@prisma/client';
 import type { ActionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { prismaClient } from '~/prismaClient';
@@ -20,36 +20,22 @@ const parsePageviewInput = async (request: Request): Promise<PageviewInput> => {
   return data;
 };
 
-export const action = async ({ request, params }: ActionArgs) => {
-  invariant(params.teamSlug, `Expected params.teamSlug`);
-  invariant(params.projectSlug, `Expected params.projectSlug`);
-
-  const data = await parsePageviewInput(request);
-  const deviceType = typeof data.screen_width === 'number' ? screenWidthToDeviceType(data.screen_width) : null;
-  const project = await prismaClient.project.findFirst({
-    where: {
-      slug: params.projectSlug,
-      teamSlug: params.teamSlug,
-    },
-  });
-
-  if (!project) {
-    return json({ errors: { name: `Can't find the given project` } }, { status: 404 });
-  }
-
-  const host = request.headers.get('host');
-  const allowedHosts = project.allowed_hosts.split(',').filter((i) => i.length);
-
-  if (allowedHosts.length > 0 && (!host || !allowedHosts.includes(host))) {
-    return json({ errors: { host: `The host is either not in headers or not declared as one of the allowed hosts` } }, { status: 404 });
-  }
-
+const getDate = (request: Request) => {
   // As long as the application is hosted on Vercel, x-vercel-ip-timezone
   // can be used to get the users timezone
   // https://vercel.com/docs/concepts/edge-network/headers#x-vercel-ip-timezone
   const timezone = request.headers.get('x-vercel-ip-timezone');
   const date = timezone ? utcToZonedTime(new Date(), timezone) : new Date();
+  return date;
+};
+
+const trackPageview = async (request: Request, project: Project) => {
+  const data = await parsePageviewInput(request);
+
+  const date = getDate(request);
   const hour = getHours(date);
+
+  const deviceType = typeof data.screen_width === 'number' ? screenWidthToDeviceType(data.screen_width) : null;
 
   const pageViewCompoundUnique: Prisma.PageViewProjectIdDateHourPathnameCompoundUniqueInput = {
     date,
@@ -88,31 +74,62 @@ export const action = async ({ request, params }: ActionArgs) => {
         : {}),
     },
   });
+};
 
-  if (project.track_page_visitors) {
-    const clientIp = getClientIPAddress(request.headers);
-    const userAgent = request.headers.get('user-agent');
-    if (clientIp) {
-      invariant(process.env.SECRET_KEY, 'Missin environment variable "SECRET_KEY"');
-      const { createHmac } = await import('crypto');
-      const hashed_user_id = createHmac('sha512', process.env.SECRET_KEY).update(`${clientIp}_${userAgent}`).digest('hex');
-      await prismaClient.pageVisitor.upsert({
-        create: {
-          projectId: project.id,
-          hashed_user_id,
-          date,
-        },
-        where: {
-          projectId_date_hashed_user_id: {
-            projectId: project.id,
-            hashed_user_id,
-            date,
-          },
-        },
-        update: {},
-      });
-    }
+const trackPageVisitor = async (request: Request, project: Project) => {
+  if (!project.track_page_visitors) {
+    return;
   }
+
+  const clientIp = getClientIPAddress(request.headers);
+  const userAgent = request.headers.get('user-agent');
+
+  if (!clientIp) {
+    return;
+  }
+
+  invariant(process.env.SECRET_KEY, 'Missin environment variable "SECRET_KEY"');
+  const { createHmac } = await import('crypto');
+  const hashed_user_id = createHmac('sha512', process.env.SECRET_KEY).update(`${clientIp}_${userAgent}`).digest('hex');
+
+  const pageVisitorCompoundUnique: Prisma.PageVisitorProjectIdDateHashed_user_idCompoundUniqueInput = {
+    projectId: project.id,
+    hashed_user_id,
+    date: getDate(request),
+  };
+
+  await prismaClient.pageVisitor.upsert({
+    create: pageVisitorCompoundUnique,
+    where: {
+      projectId_date_hashed_user_id: pageVisitorCompoundUnique,
+    },
+    update: {},
+  });
+};
+
+export const action = async ({ request, params }: ActionArgs) => {
+  invariant(params.teamSlug, `Expected params.teamSlug`);
+  invariant(params.projectSlug, `Expected params.projectSlug`);
+
+  const project = await prismaClient.project.findFirst({
+    where: {
+      slug: params.projectSlug,
+      teamSlug: params.teamSlug,
+    },
+  });
+
+  if (!project) {
+    return json({ errors: { name: `Can't find the given project` } }, { status: 404 });
+  }
+
+  const host = request.headers.get('host');
+  const allowedHosts = project.allowed_hosts.split(',').filter((i) => i.length);
+
+  if (allowedHosts.length > 0 && (!host || !allowedHosts.includes(host))) {
+    return json({ errors: { host: `The host is either not in headers or not declared as one of the allowed hosts` } }, { status: 404 });
+  }
+
+  await Promise.all([trackPageview(request, project), trackPageVisitor(request, project)]);
 
   return json({}, { status: 202 });
 };
