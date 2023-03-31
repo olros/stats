f1ac3d1a4ca8c858f1974c20da2cddfefa46e216
Olaf Rosendahl <olafrosendahl@gmail.com>
2023-03-31 12:06:26 +0200

    feat: dont use data proxy

--- web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
+++ web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
@@ -118,8 +118,6 @@ const trackPageVisitor = async (request: Request, project: Project) => {
   });
 };
 
-export const config = { runtime: 'edge' };
-
 export const action = async ({ request, params }: ActionArgs) => {
   invariant(params.teamSlug, `Expected params.teamSlug`);
   invariant(params.projectSlug, `Expected params.projectSlug`);
