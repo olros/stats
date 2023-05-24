7c6d0a462900426059ef62981846f10a048170a8
Olaf Rosendahl <olafrosendahl@gmail.com>
2023-05-24 22:35:51 +0200

    feat: use pageview edge endpoints

--- web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
+++ web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
@@ -1,136 +1,35 @@
-import type { Prisma, Project } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { RequestContext } from '@vercel/edge';
 import type { ActionArgs } from '@vercel/remix';
 import { json } from '@vercel/remix';
-import { prismaClient } from '~/prismaClient';
-import { screenWidthToDeviceType } from '~/utils';
-import { getDate, getProjectAndCheckPermissions } from '~/utils_api.server';
-import { getPageViewsUsage, getPageVisitorsUsage } from '~/utils_usage.server';
-import crypto from 'crypto';
-import { getHours } from 'date-fns';
-import { getClientIPAddress } from 'remix-utils';
 import invariant from 'tiny-invariant';
 
-import { DeviceType } from '~/types';
-import type { PageviewInput } from '~/types';
+export const config = { runtime: 'edge' };
 
-const parsePageviewInput = async (request: Request): Promise<PageviewInput> => {
-  const data = (await request.json()) as PageviewInput;
-  if (!data.pathname) {
-    throw json({ errors: { pathname: `Pathname isn't defined` } }, { status: 400 });
-  }
-
-  return data;
-};
-
-const trackPageview = async (request: Request, project: Project) => {
-  const { pageViews } = await getPageViewsUsage(project.teamSlug);
-
-  if (!pageViews.withinLimit) {
-    return;
-  }
-
-  const data = await parsePageviewInput(request);
-
-  const date = getDate(request);
-  const hour = getHours(date);
-
-  const deviceType = typeof data.screen_width === 'number' ? screenWidthToDeviceType(data.screen_width) : null;
-
-  const pageViewCompoundUnique: Prisma.PageViewProjectIdDateHourPathnameCompoundUniqueInput = {
-    date,
-    hour,
-    projectId: project.id,
-    pathname: data.pathname,
-  };
-
-  await prismaClient.pageView.upsert({
-    create: {
-      ...pageViewCompoundUnique,
-      count: 1,
-      dekstop_count: deviceType === DeviceType.DESKTOP ? 1 : 0,
-      tablet_count: deviceType === DeviceType.TABLET ? 1 : 0,
-      mobile_count: deviceType === DeviceType.MOBILE ? 1 : 0,
-    },
-    where: {
-      projectId_date_hour_pathname: pageViewCompoundUnique,
-    },
-    update: {
-      count: { increment: 1 },
-      ...(deviceType === DeviceType.DESKTOP
-        ? {
-            dekstop_count: { increment: 1 },
-          }
-        : {}),
-      ...(deviceType === DeviceType.TABLET
-        ? {
-            tablet_count: { increment: 1 },
-          }
-        : {}),
-      ...(deviceType === DeviceType.MOBILE
-        ? {
-            mobile_count: { increment: 1 },
-          }
-        : {}),
-    },
+const forwardPageview = async (request: Request, teamSlug: string, projectSlug: string) => {
+  const host = process.env.NODE_ENV === 'production' ? 'https://stats.olafros.com' : 'http://localhost:3000';
+  const body = await request.json();
+  return fetch(`${host}/api-internal/${teamSlug}/${projectSlug}/pageview/`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: request.headers,
   });
 };
 
-const hashPageVisitorIdentifier = async (clientIp: string, userAgent: string) => {
-  invariant(process.env.SECRET_KEY, 'Expected environment variable "SECRET_KEY" to be set when tracking page visitors');
-  const msgUint8 = new TextEncoder().encode(`${clientIp}_${userAgent}_${process.env.SECRET_KEY}`); // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-  return hashHex;
-};
-
-const trackPageVisitor = async (request: Request, project: Project) => {
-  if (!project.track_page_visitors) {
-    return;
-  }
-
-  const { pageVisitors } = await getPageVisitorsUsage(project.teamSlug);
-
-  if (!pageVisitors.withinLimit) {
-    return;
-  }
-
-  const clientIp = getClientIPAddress(request.headers);
-  const userAgent = request.headers.get('user-agent');
-
-  if (!clientIp || !userAgent) {
-    return;
-  }
-
-  const hashed_user_id = await hashPageVisitorIdentifier(clientIp, userAgent);
-
-  const pageVisitorCompoundUnique: Prisma.PageVisitorProjectIdDateHashed_user_idCompoundUniqueInput = {
-    projectId: project.id,
-    hashed_user_id,
-    date: getDate(request),
-  };
-
-  await prismaClient.pageVisitor.upsert({
-    create: pageVisitorCompoundUnique,
-    where: {
-      projectId_date_hashed_user_id: pageVisitorCompoundUnique,
-    },
-    update: {},
-  });
-};
-
-export const action = async ({ request, params }: ActionArgs) => {
+export const action = async ({ request, params, context }: ActionArgs) => {
   try {
     invariant(params.teamSlug, `Expected params.teamSlug`);
     invariant(params.projectSlug, `Expected params.projectSlug`);
+    const ctx = context as unknown as RequestContext;
 
-    const { project } = await getProjectAndCheckPermissions(request, params.teamSlug, params.projectSlug);
-
-    await Promise.all([trackPageview(request, project), trackPageVisitor(request, project)]);
-
-    return json({ ok: true }, { status: 202 });
+    if ('waitUntil' in ctx) {
+      ctx.waitUntil(forwardPageview(request, params.teamSlug, params.projectSlug));
+    } else {
+      await forwardPageview(request, params.teamSlug, params.projectSlug);
+    }
+    return json({ ok: true }, { status: 200 });
   } catch (e) {
     console.error(e);
-    return json({ ok: false }, { status: 400 });
+    return json({ ok: false }, { status: 200 });
   }
 };
