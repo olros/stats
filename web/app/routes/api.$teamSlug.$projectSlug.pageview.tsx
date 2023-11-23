f4154d6c880b165a05839f0545b70254f0d6f603
Olaf Rosendahl <olafrosendahl@gmail.com>
2023-11-23 14:06:48 +0100

    feat: add date index

--- web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
+++ web/app/routes/api.$teamSlug.$projectSlug.pageview.tsx
@@ -32,9 +32,8 @@ export const getPageViewUserIdHash = async (ip: string, userAgent: string, date:
 export const getPageViewUserAgentData = (ua: UserAgentData): PageviewRequestData['userAgentData'] => {
   const browser = ua.browser.name || null;
   const device = [ua.device.vendor, ua.device.model].filter(Boolean).join(' ') || null;
-  const device_size = ua.device.type || null;
   const os = ua.os.name || null;
-  return { browser, device, device_size, os };
+  return { browser, device, os };
 };
 
 const getPageViewNextRequest = async (request: Request): Promise<Request | undefined> => {
