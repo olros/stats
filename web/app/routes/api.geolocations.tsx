425d9514acd0d79aa11d6b1aeb30eddfa726bc3b
Olaf Rosendahl <olafrosendahl@gmail.com>
2024-03-15 13:50:19 +0100

    test

--- web/app/routes/api.geolocations.tsx
+++ web/app/routes/api.geolocations.tsx
@@ -1,10 +1,24 @@
 import { subHours } from 'date-fns';
-import { getTopGeoLocations } from '~/functions.server/getTopGeoLocations';
 import { json } from '@vercel/remix';
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool } from '@neondatabase/serverless';
+import { TopGeoLocationData } from '~/components/statistics/loader.server';
 
 export const config = { runtime: 'edge' };
 
 export async function loader() {
-  const geoLocations = await getTopGeoLocations(subHours(new Date(), 24));
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  const prismaClient = new PrismaClient({ adapter });
+  const geoLocations = prismaClient.$queryRaw<TopGeoLocationData[]>`
+  SELECT CONCAT(l.flag, ' ', l.city) as "name", l.latitude, l.longitude, COUNT(*)::int as "count"
+  FROM public."PageViewNext" p
+  JOIN public."Location" l ON p."locationId" = l.id
+  WHERE p.date >= ${subHours(new Date(), 24)}
+  GROUP BY l.id
+  ORDER BY "count" DESC
+  LIMIT 250;
+`;
   return json({ geoLocations });
 }
