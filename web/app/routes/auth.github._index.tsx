b081ba65fd72b5b8ccc043cb95d26f2b6f0c2f26
Olaf Rosendahl <olafrosendahl@gmail.com>
2024-04-26 13:44:37 +0200

    feat: remove stream routes

--- web/app/routes/auth.github._index.tsx
+++ web/app/routes/auth.github._index.tsx
@@ -2,6 +2,8 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from '@vercel/remix';
 import { authenticator } from '~/auth.server';
 import { redirect } from '~/utils.server';
 
+export const config = { runtime: 'edge' };
+
 export const loader = async ({ response }: LoaderFunctionArgs) => redirect(response, '/');
 
 export const action = async ({ request }: ActionFunctionArgs) =>
--- web/app/routes/auth.github.callback.tsx
+++ web/app/routes/auth.github.callback.tsx
@@ -1,6 +1,8 @@
 import type { LoaderFunctionArgs } from '@vercel/remix';
 import { authenticator } from '~/auth.server';
 
+export const config = { runtime: 'edge' };
+
 export const loader = async ({ request }: LoaderFunctionArgs) =>
   authenticator.authenticate('github', request, {
     successRedirect: '/dashboard',
